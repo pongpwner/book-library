37ef9dc86cf3ace3726c5fc8a367aeee44c27d59
EricShyu <ericsshyus@gmail.com>
2022-08-24 16:34:43 -0400

    add toggle completed button

--- src/script.js
+++ src/script.js
@@ -11,9 +11,8 @@ let book1= new Book('tolken','lotr',444, true);
 let book2= new Book('markus', 'meditations',1000, false)
 
 
-function addBookToLibrary(book){
-    myLibrary.push(book)
-}
+
+
 
 
 let domLibrary=document.querySelector('.library');
@@ -56,20 +55,79 @@ while(library.firstChild){
 
 //fills library with books on the dom
 function populateLibrary(library){
-    library.forEach(book=>{
-        let bookDom= document.createElement('li')
-        bookDom.classList.add('book')
-        let bookInfo= document.createElement('div')
-        bookInfo.textContent=`title: ${book.title}, author: ${book.author}, pages: ${book.pages}, completed:${book.completed}`
-        bookDom.appendChild(bookInfo);
-        domLibrary.appendChild(bookDom)
+    // library.forEach(book=>{
+    //     let bookDom= document.createElement('li')
+    //     bookDom.classList.add('book')
+    //     let bookInfo= document.createElement('div')
+    //     bookInfo.textContent=`title: ${book.title}, author: ${book.author}, pages: ${book.pages}, completed:${book.completed}`
+    //     bookDom.appendChild(bookInfo);
+    //     domLibrary.appendChild(bookDom)
         
-    })
+    // })
+    for(let i=0; i<library.length; i++){
+        // add container
+        let bookDom= document.createElement('li')
+        bookDom.dataset.bookId=i;
+        bookDom.classList.add('book')
+
+        //add book info
+        let bookInfo= document.createElement('div')
+        bookInfo.textContent=`title: ${library[i].title}, author: ${library[i].author}, pages: ${library[i].pages}, completed:${library[i].completed}`
+        bookDom.appendChild(bookInfo);
+
+        //add remove button
+        let removeButton= document.createElement('button')
+        removeButton.type="button"
+        removeButton.textContent='Remove Book'
+        removeButton.addEventListener('click',()=>removeBook(i,myLibrary,domLibrary))
+        bookDom.appendChild(removeButton)
+
+        //add toggle completed
+        let toggleButton= document.createElement('button')
+        toggleButton.type="button"
+        toggleButton.textContent='toggle complete'
+        toggleButton.addEventListener('click',()=>toggleRead(i,myLibrary,domLibrary))
+        bookDom.appendChild(toggleButton)
+
+        domLibrary.appendChild(bookDom)
+    }
+
+
+}
+//
+function toggleRead(id,library, domLibrary){
+
+
+    for(let i=0; i<myLibrary.length;i++){
+        if(i===id){
+           
+            if(myLibrary[i].completed==='yes'){
+                myLibrary[i].completed='no'
+              
+            }
+            else if(myLibrary[i].completed==='no'){
+                myLibrary[i].completed='yes'
+            
+            }
+    }
+    console.log(myLibrary)
+
+  //updates dom and array
+  removeAllBooks(domLibrary)
+  populateLibrary(library)
+
+}
+}
+//removes a single book
+function removeBook(bookId, library,domLibrary){
+    library.splice(bookId,1)
+    //updates dom and array
+    removeAllBooks(domLibrary)
+    populateLibrary(library)
+    
 
 }
 
-addBookToLibrary(book1)
-addBookToLibrary(book2)
 populateLibrary(myLibrary)
 
 
